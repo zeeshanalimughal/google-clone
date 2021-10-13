367a04bb042095aee14b7c6f5bceeaaf60a8e4cc
Zeeshan Ali <zeeshanaliryk18@gmail.com>
2021-10-13 14:36:42 +0500

    make dynamic date change in footer component

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,4 @@
-import { GlobeIcon } from '@heroicons/react/solid'
+import { GlobeAltIcon } from '@heroicons/react/solid'
 function Footer() {
     return (
         <footer className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100 text-sm text-gray-500">
@@ -7,7 +7,7 @@ function Footer() {
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense px-8 py-3">
                 <div className="flex justify-center items-center md:col-span-2 lg:col-span-1 lg:col-start-2">
-                 <GlobeIcon className="h-5 text-green-700"/>   Carbon neutral since 2007
+                 <GlobeAltIcon className="h-5 text-green-700"/>   All Rights Reserved | Zeesha Ali {new Date().getFullYear()}
                 </div>
                 <div className="flex justify-center space-x-8 whitespace-nowrap md:justify-self-start">
                     <p>Advertising</p>
