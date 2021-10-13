23b9d7b11517c66030ed3c05fc946f3e82a1933c
Zeeshan Ali <zeeshanaliryk18@gmail.com>
2021-10-13 14:26:15 +0500

    fix some error in pages/index

--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ const router = useRouter();
 
         <div className="flex flex-col w-1/2  space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
           <button onClick={search} className="btn">Google Search</button>
-          <button onClick={search} className="btn">I'am Feeling Luckey</button>
+          <button onClick={search} className="btn">I am Feeling Luckey</button>
         </div>
       </form>
 
